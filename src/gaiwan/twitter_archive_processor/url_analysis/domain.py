d54ad192591585672f011e19e690acb5a4ce3a68
PeterGrazi <peter.m.graziano@gmail.com>
2025-02-19 14:56:08 -0500

    domain tests pass.

--- src/gaiwan/twitter_archive_processor/url_analysis/domain.py
+++ src/gaiwan/twitter_archive_processor/url_analysis/domain.py
@@ -6,7 +6,7 @@ class DomainNormalizer:
     
     def __init__(self):
         self.domain_mappings = {
-            'twitter.com': ['twitter.com', 'x.com', 'www.twitter.com', 'm.twitter.com'],
+            'twitter.com': ['twitter.com', 'www.twitter.com', 'm.twitter.com'],
             'youtube.com': ['youtube.com', 'www.youtube.com', 'youtu.be', 'm.youtube.com'],
             'wikipedia.org': ['wikipedia.org', 'en.wikipedia.org', 'fr.wikipedia.org', 'de.wikipedia.org'],
             'github.com': ['github.com', 'raw.githubusercontent.com', 'gist.github.com'],
@@ -26,10 +26,24 @@ class DomainNormalizer:
         domain = domain.lower()
         if domain.startswith('www.'):
             domain = domain[4:]
-        if 'youtube.com' in domain or 'youtu.be' in domain:
-            return 'youtube.com'
-        if 'twitter.com' in domain or 'x.com' in domain:
+            
+        # Special case for Twitter/X domains
+        if domain == 'x.com' or domain.endswith('.x.com') or domain == 'twitter.com' or domain.endswith('.twitter.com'):
             return 'twitter.com'
+            
+        # Check each mapping
+        for normalized, variants in self.domain_mappings.items():
+            if isinstance(variants, list):
+                if domain in variants:
+                    return normalized
+            elif callable(variants):
+                if variants(domain):
+                    return normalized
+            
+            # Special case for Wikipedia language subdomains
+            if normalized == 'wikipedia.org' and domain.endswith('.wikipedia.org'):
+                return normalized
+                
         return domain
 
     def is_shortener(self, domain: str) -> bool:
